251f954e3f75327efc5b11cb900fbdc8b2e77be2
adonaiviedondon <vieiraadonai@gmail.com>
2026-01-08 02:08:18 -0300

    Update RegistroAdmin.jsx

--- Front-End/src/paginas/RegistroAdmin.jsx
+++ Front-End/src/paginas/RegistroAdmin.jsx
@@ -1,13 +1,48 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-// export default function RegistroAdmin() {
-//     const navigate = useNavigate();
 
-//     const [form, setForm] = useState({
-//     nome: "",
-//     email: "",
-//     senha: "",
-//     cargo: "",
-//   });
-// }
+export default function RegistroAdmin() {
+    
+
+    const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    cargo: "",
+  });
+
+  function handleChange(e) {
+    setForm({...form, [e.target.name]: e.target.value})
+    
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const payload = {
+      tipoUsuario: "ADMIN",
+      login: form.login,
+      senha: form.senha,
+      cpf: form.cpf,
+      idade: Number(form.idade),
+      sexo: form.sexo,
+      lugaresOfertados: form.lugaresOfertados,
+    };
+
+    console.log(payload);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Cadastro Administrador</h1>
+
+      <input name="login" placeholder="Email" onChange={handleChange} />
+      <input name="senha" type="password" placeholder="Senha" onChange={handleChange} />
+      <input name="cpf" placeholder="CPF" onChange={handleChange} />
+      <input name="idade" type="number" placeholder="Idade" onChange={handleChange} />
+      <input name="sexo" placeholder="Sexo" onChange={handleChange} />
+      <input name="lugaresOfertados" placeholder="Lugares ofertados" onChange={handleChange} />
+
+      <button>Cadastrar</button>
+    </form>
+  );
+}
